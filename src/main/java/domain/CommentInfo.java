b21cd8b533dde8759049bd10e53fd13ee6666b62
evgenijerm <erm.evgeniy18@gmail.com>
2020-10-02 13:02:49 +0300

    v2

--- src/main/java/domain/CommentInfo.java
+++ src/main/java/domain/CommentInfo.java
@@ -4,6 +4,52 @@ public class CommentInfo {
     private int id;
     private String title;
     private String description;
+    private int owner_id;
+    private int from_id;
+    private int date;
+    private String text;
+    private int friends_only;
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public int getFrom_id() {
+        return from_id;
+    }
+
+    public void setFrom_id(int from_id) {
+        this.from_id = from_id;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getFriends_only() {
+        return friends_only;
+    }
+
+    public void setFriends_only(int friends_only) {
+        this.friends_only = friends_only;
+    }
+
 
     public int getId() {
         return id;
--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -8,6 +8,11 @@ public class Main {
         commentInfo.setId(111);
         commentInfo.setTitle("Заголовок");
         commentInfo.setDescription("Описание");
+        commentInfo.setOwner_id(555);
+        commentInfo.setFrom_id(115);
+        commentInfo.setDate(1601632220);
+        commentInfo.setText("пост от 1 октября");
+        commentInfo.setFriends_only(1);
 
         Post post = new Post();
         post.setId(1);
