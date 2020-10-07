f59f3da32afdf04a567ad83113efd08c9a27b7e9
evgenijerm <erm.evgeniy18@gmail.com>
2020-10-07 17:02:26 +0300

    доработанное задание

--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -2,6 +2,14 @@ package domain;
 
 public class Post {
     private int id;
+    private String CommentInfo;
+    private String autor;
+    private int date;
+    private int likes;
+    private int reposts;
+    private int share;
+    private int comments;
+    private int views;
 
     public int getId() {
         return id;
@@ -11,13 +19,68 @@ public class Post {
         this.id = id;
     }
 
-    public CommentInfo getCommentsInfo() {
-        return commentsInfo;
+    public String getCommentInfo() {
+        return CommentInfo;
     }
 
-    public void setCommentsInfo(CommentInfo commentsInfo) {
-        this.commentsInfo = commentsInfo;
+    public void setCommentInfo(String commentInfo) {
+        CommentInfo = commentInfo;
     }
 
-    private CommentInfo commentsInfo;
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getReposts() {
+        return reposts;
+    }
+
+    public void setReposts(int reposts) {
+        this.reposts = reposts;
+    }
+
+    public int getShare() {
+        return share;
+    }
+
+    public void setShare(int share) {
+        this.share = share;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public void setComments(int comments) {
+        this.comments = comments;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
 }
---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public String searchForAnEntry() {
+        return null;
+    }
+    public String deleteRecord() {
+        return null;
+    }
+}
